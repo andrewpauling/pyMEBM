889515f73245b8f8f00cfe2444eedec029a5ee1e
andrewpauling <apauling@uw.edu>
2019-07-25 14:07:52 -0700

    Clean up comments, add params

--- ebm.py
+++ ebm.py
@@ -29,44 +29,56 @@ class EBM(object):
     """
     This class sets up an energy balance model with diffusion of either
     sensible heat or moist static energy, as in Roe et al., 2015.
-    
+
     **Initialization parameters** \n
 
     An instance of ``EBM`` is initialized with the following
     arguments *(for detailed information see Object attributes below)*:
 
-    :param str diffusion:       string that sets the type of diffusion to be 
+    :param str diffusion:       string that sets the type of diffusion to be
                                 used. Valid values: 'dry' or 'moist'
-                                
+
                                 - default value: ``'moist'``
-                                
+
     :param float D:             value of the diffusion coefficient \n
                                 unit: :math:`\\textrm{W m}^{-2}`
-                                
+
                                 - default value: ``0.2598 (moist), 0.44 (dry)``
-                                
+
     :param float Q0:            value of the solar insolation    \n
                                 unit: :math:`\\textrm{W m}^{-2}`
-                                
+
                                 -default value: ``342.0``
-    
+
     :param float A0:            value of longwave parameter A in A + BT  \n
                                 unit: :math:`\\textrm{W m}^{-2}`
-                                
+
                                 -default value: ``207.3``
-                                
+
     :param float B0:            value of longwave parameter B in A + BT  \n
                                 unit: :math:`\\textrm{W m}^{-2}\\textrm{K}^{-1}`
-                                
+
                                 -default value: ``2.09``
                                 
+    :param float alb_ice:       value of ice albedo  \n
+                                unit: ``unitless``
+
+                                -default value: ``0.55``
+                                
+    :param float alb_noice:     value of non-ice albedo  \n
+                                unit: ``unitless``
+
+                                -default value: ``0.3``
+
     """
     def __init__(self,
                  diffusion='moist',
                  D=None,
                  Q0=342.0,
                  A0=207.3,
-                 B0=2.09):
+                 B0=2.09,
+                 alb_ice=0.55,
+                 alb_noice=0.3):
 
         self.timestep = 1./500000
         self.itermax = 10000000
@@ -110,8 +122,8 @@ class EBM(object):
         self.param['Q0'] = Q0     # [W m-2]  solar constant n.b. 4Q0 = 1368
         self.param['A0'] = A0     # Size of longwave cooling constant [W/m2]
         self.param['B0'] = B0     # [W m-2 degC-1] OLR response NOTE UNITS
-        self.param['alb_noice'] = 0.3  # ice free albedo.
-        self.param['alb_ice'] = 0.55   # ice covered albedo.
+        self.param['alb_noice'] = alb_noice  # ice free albedo.
+        self.param['alb_ice'] = alb_ice      # ice covered albedo.
         self.param['Cl'] = 0.2    # units: J /(m2 K)
 
         self.const = AttrDict()
@@ -139,17 +151,13 @@ class EBM(object):
         return '\n'.join(summary)
 
     def _create_xvec(self):
-        x = np.arange(-1.0+self.delx/2, 1.0, self.delx)
-        return x
+        return np.arange(-1.0+self.delx/2, 1.0, self.delx)
 
     def _create_div_matrix(self):
-
         # Create matrix to take a divergence of something it acts on
         # set up lambda array.
-        lam = (1-np.square(np.arange(-1.0, 1.0+self.delx, self.delx))) / \
-            np.square(self.delx)
-
-        lam = np.multiply(self.D, lam)
+        xl = np.arange(-1.0, 1.0+self.delx, self.delx)
+        lam = self.D*(1-np.square(xl))/np.square(self.delx)
 
         M = np.zeros((self.nx, self.nx))
 
@@ -169,16 +177,17 @@ class EBM(object):
         return M  # Divergence matrix
 
     def _create_t_profile(self):
-        tprof = 0.5*(1-1*np.square(self.x))
-        return tprof
+        return 0.5*(1-1*self.x*self.x)
 
     def compute(self):
 
-        A = self.param.A0
-        B = self.param.B0*np.ones(self.x.size)  # longwave cooling [W/(m2 K)]
+        A = self.param['A0']
+        B = self.param['B0']*np.ones(self.x.size)  # longwave cooling [W/(m2 K)]
 
         # set up inital T profile
         T = self._create_t_profile()
+        
+        # Pre-compute some constants for the loop
         alf0 = self.param.alb_noice*np.ones(self.x.size)
         q0 = self.const['eps']*self.param['relhum']/self.const['psfc'] * \
             self.const['e0']
@@ -195,12 +204,7 @@ class EBM(object):
         while imbalsum > 0.1:
 
             # Calculate Source  (ASR) for this loop:
-
             alf = np.where(T <= -10, self.param['alb_ice'], alf0)
-#            for idx, item in enumerate(T):
-#                if item <= -10:
-#                    alf[idx] = self.param.alf_ice
-
             Src = Src0*(1-alf)
 
             # spec. hum g/kg, and theta_e
@@ -211,13 +215,11 @@ class EBM(object):
 
             # Calculate new T from Source and Sink terms.
             # Diffuse moist static energy (theta_e)
-
             dT = dT0*(Src - A - (B*T) + np.matmul(self.M, theta_e))
 
             T += dT
 
             imbal = (Src - A - B*T)
-
             imbalsum = imbal.sum()
 
         print('Tmean = '+str(np.mean(T)))
