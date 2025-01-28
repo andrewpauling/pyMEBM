83626a2122bfba0b2bcdf73cd46a0b45881ade18
Andrew Pauling <andrewpauling109@gmail.com>
2025-01-29 11:11:50 +1300

    remove use of deprecated attrdict

--- pyMEBM/ebm.py
+++ pyMEBM/ebm.py
@@ -12,7 +12,6 @@ from scipy.interpolate import griddata
 import matplotlib.pyplot as plt
 import pickle as pkl
 import time
-from attrdict import AttrDict
 from numba import njit
 
 # This code solves the energy balance model used in
@@ -72,113 +71,114 @@ class EBM(object):
                                 -default value: ``0.3``
 
     """
-    def __init__(self,
-                 diffusion='moist',
-                 D=None,
-                 Q0=342.0,
-                 A0=207.3,
-                 B0=2.09,
-                 alb_ice=0.55,
-                 alb_noice=0.3):
 
-        self.timestep = 1./50000
+    def __init__(
+        self,
+        diffusion="moist",
+        D=None,
+        Q0=342.0,
+        A0=207.3,
+        B0=2.09,
+        alb_ice=0.55,
+        alb_noice=0.3,
+    ):
+
+        self.timestep = 1.0 / 50000
         self.nmax = 60000
 
         # Define grid spacing
         self.nx = 101
-        self.delx = 2.0/self.nx
+        self.delx = 2.0 / self.nx
 
         self.x = self._create_xvec()
-        self.phi = np.arcsin(self.x)*180/np.pi
-        
-                # initialize diffusion scheme
+        self.phi = np.arcsin(self.x) * 180 / np.pi
+
+        # initialize diffusion scheme
         self.diffusion = diffusion
-        
-        self.param = AttrDict()
-        
+
+        self.param = {}
+
         # Initialize diffusion parameter
-        if self.diffusion == 'moist':
+        if self.diffusion == "moist":
             if D is not None:
-                self.param['D'] = D
+                self.param["D"] = D
             else:
-                self.param['D'] = 0.2598  # W/(m2 K)
+                self.param["D"] = 0.2598  # W/(m2 K)
 
-            self.param['relhum'] = 0.8   # relative humidity
-        elif self.diffusion == 'dry':
+            self.param["relhum"] = 0.8  # relative humidity
+        elif self.diffusion == "dry":
             if D is not None:
-                self.param['D'] = D
+                self.param["D"] = D
             else:
-                self.param['D'] = 0.44  # W/(m2 K)
+                self.param["D"] = 0.44  # W/(m2 K)
 
-            self.param['relhum'] = 0.
+            self.param["relhum"] = 0.0
         else:
-            raise ValueError(
-                    "Invalid diffusion scheme: '{}'".format(self.diffusion)
-                    )
+            raise ValueError("Invalid diffusion scheme: '{}'".format(self.diffusion))
 
         # climate parameters
         # I think this C = rho * c * h_ml /(pi*1e7).
         # this is consistent with a ~1m layer of dirt
         # Note - heat capacity over LAND for fast convergence
-        self.param['Q0'] = Q0     # [W m-2]  solar constant n.b. 4Q0 = 1368
-        self.param['A0'] = A0     # Size of longwave cooling constant [W/m2]
-        self.param['B0'] = B0     # [W m-2 degC-1] OLR response NOTE UNITS
-        self.param['alb_noice'] = alb_noice  # ice free albedo.
-        self.param['alb_ice'] = alb_ice      # ice covered albedo.
-        self.param['Cl'] = 0.2    # units: J /(m2 K)
+        self.param["Q0"] = Q0  # [W m-2]  solar constant n.b. 4Q0 = 1368
+        self.param["A0"] = A0  # Size of longwave cooling constant [W/m2]
+        self.param["B0"] = B0  # [W m-2 degC-1] OLR response NOTE UNITS
+        self.param["alb_noice"] = alb_noice  # ice free albedo.
+        self.param["alb_ice"] = alb_ice  # ice covered albedo.
+        self.param["Cl"] = 0.2  # units: J /(m2 K)
 
-        self.const = AttrDict()
+        self.const = {}
         # Moisture parameters
-        self.const['eps'] = 0.622    # moisture coonstant
-        self.const['psfc'] = 9.8e4   # (Pa)
-        self.const['e0'] = 611.2     # vap. press (Pa)
-        self.const['a'] = 17.67      # sat vap constants
-        self.const['b'] = 243.5      # !!T must be in temperature
-        self.const['L'] = 2.45e6     # latent heat of vaporization (J kg-1)
-        self.const['cp'] = 1004      # (J kg-1 K-1)
+        self.const["eps"] = 0.622  # moisture coonstant
+        self.const["psfc"] = 9.8e4  # (Pa)
+        self.const["e0"] = 611.2  # vap. press (Pa)
+        self.const["a"] = 17.67  # sat vap constants
+        self.const["b"] = 243.5  # !!T must be in temperature
+        self.const["L"] = 2.45e6  # latent heat of vaporization (J kg-1)
+        self.const["cp"] = 1004  # (J kg-1 K-1)
 
         # magnitude of diffusivity
-        self.D = self.param['D']*np.ones(self.nx+1)  # diffusivity for MSE
+        self.D = self.param["D"] * np.ones(self.nx + 1)  # diffusivity for MSE
 
         self.M = self._create_div_matrix()
 
         self.variables = AttrDict()
-        
+
         self.variables.theta_e = np.zeros(self.nx)
         self.variables.q = np.zeros(self.nx)
 
     def __repr__(self):
-        summary = ['<pyMEBM.{}>'.format(type(self).__name__)]
+        summary = ["<pyMEBM.{}>".format(type(self).__name__)]
         summary.append("diffusion: '{}'".format(self.diffusion))
         summary.append("---Parameters---")
         for key, item in self.param.items():
-            if key != 'relhum' and key != 'Cl':
-                summary.append('{}: {}'.format(key, item))
-        return '\n'.join(summary)
+            if key != "relhum" and key != "Cl":
+                summary.append("{}: {}".format(key, item))
+        return "\n".join(summary)
 
     def _create_xvec(self):
-        return np.arange(-1.0+self.delx/2, 1.0, self.delx)
+        return np.arange(-1.0 + self.delx / 2, 1.0, self.delx)
 
     def _create_div_matrix(self):
         # Create matrix to take a divergence of something it acts on
         # set up lambda array.
-        xl = np.arange(-1.0, 1.0+self.delx, self.delx)
-        lam = self.D*(1-np.square(xl))/np.square(self.delx)
+        xl = np.arange(-1.0, 1.0 + self.delx, self.delx)
+        lam = self.D * (1 - np.square(xl)) / np.square(self.delx)
 
         M = np.zeros((self.nx, self.nx))
 
         M[0, 0] = -lam[1]
         M[0, 1] = lam[1]
 
-        M[self.nx-1, self.nx-2] = lam[self.nx-1]
-        M[self.nx-1, self.nx-1] = -lam[self.nx-1]
+        M[self.nx - 1, self.nx - 2] = lam[self.nx - 1]
+        M[self.nx - 1, self.nx - 1] = -lam[self.nx - 1]
 
-        for j in range(1, self.nx-1):
-            M[j, j-1] = lam[j]
+        for j in range(1, self.nx - 1):
+            M[j, j - 1] = lam[j]
 
-            M[j, j] = -(lam[j+1]+lam[j])
+            M[j, j] = -(lam[j + 1] + lam[j])
 
-            M[j, j+1] = lam[j+1]
+            M[j, j + 1] = lam[j + 1]
 
         return M  # Divergence matrix
 
@@ -186,29 +186,33 @@ class EBM(object):
         """
         Update temperature given theta_e and Src
         """
-        dT0 = self.timestep/self.param['Cl']
-        frc = Src - self.param['A0'] - (self.param['B0']*self.variables['T'])
-        dT = dT0*(frc + np.matmul(self.M, theta_e))
+        dT0 = self.timestep / self.param["Cl"]
+        frc = Src - self.param["A0"] - (self.param["B0"] * self.variables["T"])
+        dT = dT0 * (frc + np.matmul(self.M, theta_e))
 
-        self.variables['T'] += dT
+        self.variables["T"] += dT
 
     def compute(self):
 
-        A = self.param['A0']
-        B = self.param['B0']*np.ones(self.x.size)
+        A = self.param["A0"]
+        B = self.param["B0"] * np.ones(self.x.size)
         # set up inital T profile
         T = self._create_t_profile()
 
         # Pre-compute some constants for the loop
-        alf0 = self.param.alb_noice*np.ones(self.x.size)
-        q0 = self.const['eps']*self.param['relhum']/self.const['psfc'] * \
-            self.const['e0']
-        dT0 = self.timestep/self.param['Cl']
-        Src0 = self.param['Q0']*(1-0.241*(3*self.x*self.x-1))
+        alf0 = self.param.alb_noice * np.ones(self.x.size)
+        q0 = (
+            self.const["eps"]
+            * self.param["relhum"]
+            / self.const["psfc"]
+            * self.const["e0"]
+        )
+        dT0 = self.timestep / self.param["Cl"]
+        Src0 = self.param["Q0"] * (1 - 0.241 * (3 * self.x * self.x - 1))
 
-        Src = Src0*(1-alf0)
+        Src = Src0 * (1 - alf0)
 
-        imbal = (Src - A - B*T)
+        imbal = Src - A - B * T
         imbalsum = imbal.mean()
 
         # Timestepping loop
@@ -216,32 +220,35 @@ class EBM(object):
         while imbalsum > 0.1:
 
             # Calculate Source  (ASR) for this loop:
-            alf = np.where(T <= -10, self.param['alb_ice'], alf0)
-            Src = Src0*(1-alf)
+            alf = np.where(T <= -10, self.param["alb_ice"], alf0)
+            Src = Src0 * (1 - alf)
 
             # spec. hum g/kg, and theta_e
-            q = q0*np.exp(self.const['a']*T/(self.const['b']+T))
+            q = q0 * np.exp(self.const["a"] * T / (self.const["b"] + T))
 
-            theta_e = 1/self.const['cp']*(self.const['cp']*(T+273.15) +
-                                          self.const['L']*q)
+            theta_e = (
+                1
+                / self.const["cp"]
+                * (self.const["cp"] * (T + 273.15) + self.const["L"] * q)
+            )
 
             # Calculate new T from Source and Sink terms.
             # Diffuse moist static energy (theta_e)
-            dT = dT0*(Src - A - (B*T) + np.matmul(self.M, theta_e))
+            dT = dT0 * (Src - A - (B * T) + np.matmul(self.M, theta_e))
 
             T += dT
 
-            imbal = (Src - A - B*T)
+            imbal = Src - A - B * T
             imbalsum = imbal.sum()
 
-        print('Tmean = '+str(np.mean(T)))
-        print('Fglob = '+str(imbal))
+        print("Tmean = " + str(np.mean(T)))
+        print("Fglob = " + str(imbal))
 
         end = time.time()
-        print('time = ' + str(end - start))
+        print("time = " + str(end - start))
 
         divF = -np.matmul(self.M, theta_e)
-        h = theta_e*self.const['cp']
+        h = theta_e * self.const["cp"]
 
         self.T = T
         self.h = h
@@ -253,26 +260,25 @@ class EBM(object):
     def plot(self):
 
         # load matlab file as a dictionary with some metadata
-        matLabData = io.loadmat('ERAtemperature.mat')
+        matLabData = io.loadmat("ERAtemperature.mat")
         matLabData.keys()  # tell me what's in the dictionary
-        lat = np.asarray(matLabData['lat'])
-        t = np.asarray(matLabData['T'])
+        lat = np.asarray(matLabData["lat"])
+        t = np.asarray(matLabData["T"])
         fig = plt.figure(1)
-        plt.plot(self.x, self.variables['T'])
+        plt.plot(self.x, self.variables["T"])
         plt.plot(np.sin(np.deg2rad(lat)), t)
-        plt.xlabel('sin(lat)', fontsize=14)
-        plt.ylabel('Temperature', fontsize=14)
-        plt.legend(('Model', 'Obs'))
+        plt.xlabel("sin(lat)", fontsize=14)
+        plt.ylabel("Temperature", fontsize=14)
+        plt.legend(("Model", "Obs"))
         plt.show()
 
         fig2 = plt.figure(2)
-        plt.plot(self.x, self.variables['theta_e'])
-        plt.xlabel('sin(lat)', fontsize=14)
-        plt.ylabel('MSE (J/kg)', fontsize=14)
+        plt.plot(self.x, self.variables["theta_e"])
+        plt.xlabel("sin(lat)", fontsize=14)
+        plt.ylabel("MSE (J/kg)", fontsize=14)
         plt.show()
 
         plt.plot(self.x, self.divF)
-        plt.xlabel('sin(lat)', fontsize=14)
-        plt.ylabel('Terms in the energy balance (W/m2)', fontsize=14)
+        plt.xlabel("sin(lat)", fontsize=14)
+        plt.ylabel("Terms in the energy balance (W/m2)", fontsize=14)
         plt.show()
-        
--- pyMEBM/ebm_perturbation.py
+++ pyMEBM/ebm_perturbation.py
@@ -9,7 +9,6 @@ Created on Thu Jul 25 16:49:34 2019
 import numpy as np
 import time
 import matplotlib.pyplot as plt
-from attrdict import AttrDict
 import scipy.io as io
 from scipy.interpolate import griddata
 
@@ -66,36 +65,35 @@ class EBMPert(EBM):
 
     :param str feedbacks:       flag to specify feedbacks to be used. Options
                                 for now are 'flat' (default) or 'CMIP5'
-                                
+
     :param bool ocn_heat_uptake: flag to include ocean heat uptake as part of
                                 forcing. False by default
-                                
+
     """
-    def __init__(self,
-                 ocn_heat_uptake=False,
-                 forcing='flat',
-                 feedbacks='flat',                 
-                 **kwargs):
-        
+
+    def __init__(
+        self, ocn_heat_uptake=False, forcing="flat", feedbacks="flat", **kwargs
+    ):
+
         self.ocn_heat_uptake = ocn_heat_uptake
         self.feedbacks = feedbacks
         self.forcing = forcing
 
         super(EBMPert, self).__init__(**kwargs)
-        
-        self.variables['T'] = 5*np.ones(self.nx)
-        
+
+        self.variables["T"] = 5 * np.ones(self.nx)
+
         # Initialize forcing and feedback parameters
         self._load_forcing_feedbacks()
         self._load_ctrl_data()
 
     def __repr__(self):
         summary = [super(EBMPert, self).__repr__()]
-        summary.append('---Configuration___')
-        summary.append('forcing: {}'.format(self.forcing))
-        summary.append('feedbacks: {}'.format(self.feedbacks))
-        summary.append('ocn_heat_uptake: {}'.format(self.ocn_heat_uptake))
-        return '\n'.join(summary)
+        summary.append("---Configuration___")
+        summary.append("forcing: {}".format(self.forcing))
+        summary.append("feedbacks: {}".format(self.feedbacks))
+        summary.append("ocn_heat_uptake: {}".format(self.ocn_heat_uptake))
+        return "\n".join(summary)
 
     def _load_ctrl_data(self):
         """
@@ -107,24 +105,30 @@ class EBMPert(EBM):
 
         """
         # loads matlab file as a dictionary with some metadata
-        matLabData = io.loadmat('ERAtemperature.mat')
+        matLabData = io.loadmat("ERAtemperature.mat")
 
-        lat = np.asarray(matLabData['lat'])
-        T_ctrl = np.asarray(matLabData['T'])
+        lat = np.asarray(matLabData["lat"])
+        T_ctrl = np.asarray(matLabData["T"])
         # average N & S hemispheres for symmetry
-        T_ctrl = 0.5*(T_ctrl+np.flipud(T_ctrl))
-        T_ctrl = griddata(np.sin(np.deg2rad(lat)), T_ctrl, self.x,
-                          method='linear')
+        T_ctrl = 0.5 * (T_ctrl + np.flipud(T_ctrl))
+        T_ctrl = griddata(np.sin(np.deg2rad(lat)), T_ctrl, self.x, method="linear")
         T_ctrl = np.squeeze(T_ctrl, axis=1)
         # here T is in oC. q is g kg-1
-        q_ctrl = self.const['eps']*self.param['relhum']/self.const['psfc'] *\
-            self.const['e0']*np.exp(
-                    self.const['a']*(T_ctrl)/(self.const['b']+(T_ctrl)))
-        theta_e_ctrl = 1/self.const['cp'] * \
-            (self.const['cp']*((T_ctrl)+273.15) + self.const['L']*q_ctrl)
+        q_ctrl = (
+            self.const["eps"]
+            * self.param["relhum"]
+            / self.const["psfc"]
+            * self.const["e0"]
+            * np.exp(self.const["a"] * (T_ctrl) / (self.const["b"] + (T_ctrl)))
+        )
+        theta_e_ctrl = (
+            1
+            / self.const["cp"]
+            * (self.const["cp"] * ((T_ctrl) + 273.15) + self.const["L"] * q_ctrl)
+        )
 
-        self.variables['T_ctrl'] = T_ctrl
-        self.variables['theta_e_ctrl'] = theta_e_ctrl
+        self.variables["T_ctrl"] = T_ctrl
+        self.variables["theta_e_ctrl"] = theta_e_ctrl
 
     def _load_forcing_feedbacks(self):
         """
@@ -138,15 +142,15 @@ class EBMPert(EBM):
         # CMIP5 ensemble-mean feedback and forcing values from 4xCO2
         # simulations (taken at year 100)
         # feedback, forcing and heat uptake for 11 models
-        matLabData = io.loadmat('CMIP5_Rf_G_lambda.mat')
-        CMIP5_lat = np.asarray(matLabData['CMIP5_lat'])  # latitude
-        CMIP5_T = np.asarray(matLabData['CMIP5_T'])  # Temperature change
-        CMIP5_lambda = np.asarray(matLabData['CMIP5_lambda'])  # feedbacks
-        CMIP5_Rf = np.asarray(matLabData['CMIP5_Rf'])  # Radiative forcing
-        CMIP5_G = np.asarray(matLabData['CMIP5_G'])  # Ocean heat uptake
-        CMIP5_names = np.asarray(matLabData['CMIP5_names'])
+        matLabData = io.loadmat("CMIP5_Rf_G_lambda.mat")
+        CMIP5_lat = np.asarray(matLabData["CMIP5_lat"])  # latitude
+        CMIP5_T = np.asarray(matLabData["CMIP5_T"])  # Temperature change
+        CMIP5_lambda = np.asarray(matLabData["CMIP5_lambda"])  # feedbacks
+        CMIP5_Rf = np.asarray(matLabData["CMIP5_Rf"])  # Radiative forcing
+        CMIP5_G = np.asarray(matLabData["CMIP5_G"])  # Ocean heat uptake
+        CMIP5_names = np.asarray(matLabData["CMIP5_names"])
 
-        if self.forcing == 'flat':
+        if self.forcing == "flat":
             # uniform forcing in [W/m2] for a quadrupling of CO2, 7.8 value
             # taken as global average of CMIP5
             R_frc = 7.8  # W/m^2
@@ -156,15 +160,15 @@ class EBMPert(EBM):
             else:
                 frc = CMIP5_Rf
 
-            R_frc = griddata(CMIP5_lat, np.mean(frc, axis=1), self.phi,
-                             method='linear')
+            R_frc = griddata(CMIP5_lat, np.mean(frc, axis=1), self.phi, method="linear")
 
-        if self.feedbacks == 'flat':
+        if self.feedbacks == "flat":
             # uniform feedback [W/m2/K], 1.4 value taken as average of CMIP5
-            B = 1.4*np.ones(self.nx)
+            B = 1.4 * np.ones(self.nx)
         else:
-            B = -griddata(CMIP5_lat, np.mean(CMIP5_lambda, axis=1), self.phi,
-                          method='linear')  # taking average over CMIP5 models
+            B = -griddata(
+                CMIP5_lat, np.mean(CMIP5_lambda, axis=1), self.phi, method="linear"
+            )  # taking average over CMIP5 models
 
         self.R_frc = R_frc
         self.lamparam = B
@@ -180,23 +184,34 @@ class EBMPert(EBM):
         """
 
         # Pre-compute some constants for the loop
-        q0 = self.const['eps']*self.param['relhum']/self.const['psfc'] * \
-            self.const['e0']
+        q0 = (
+            self.const["eps"]
+            * self.param["relhum"]
+            / self.const["psfc"]
+            * self.const["e0"]
+        )
 
         # spec. hum g/kg, and theta_e
-        self.variables['q'] = q0 * \
-            np.exp(self.const['a']*(self.variables['T']+self.variables['T_ctrl']) /
-                   (self.const['b']+self.variables['T']+self.variables['T_ctrl']))
+        self.variables["q"] = q0 * np.exp(
+            self.const["a"]
+            * (self.variables["T"] + self.variables["T_ctrl"])
+            / (self.const["b"] + self.variables["T"] + self.variables["T_ctrl"])
+        )
 
-        self.variables['theta_e'] = 1/self.const['cp'] *\
-            (self.const['cp']*(self.variables['T']+self.variables['T_ctrl']+273.15) +
-             self.const['L']*self.variables['q'])
+        self.variables["theta_e"] = (
+            1
+            / self.const["cp"]
+            * (
+                self.const["cp"]
+                * (self.variables["T"] + self.variables["T_ctrl"] + 273.15)
+                + self.const["L"] * self.variables["q"]
+            )
+        )
 
-        theta_e_pert = self.variables['theta_e'] - self.variables['theta_e_ctrl']
+        theta_e_pert = self.variables["theta_e"] - self.variables["theta_e_ctrl"]
 
-        Src = self.param['A0'] + \
-            self.param['B0']*(self.variables['T'])
-        Src += self.R_frc - self.lamparam*self.variables['T']
+        Src = self.param["A0"] + self.param["B0"] * (self.variables["T"])
+        Src += self.R_frc - self.lamparam * self.variables["T"]
 
         super(EBMPert, self).step_forward(theta_e_pert, Src)
 
@@ -209,13 +224,13 @@ class EBMPert(EBM):
         None.
 
         """
-        imbal = (self.R_frc - self.lamparam*self.variables['T']).mean()
+        imbal = (self.R_frc - self.lamparam * self.variables["T"]).mean()
 
-        print('Integrating to convergence...')
+        print("Integrating to convergence...")
         start = time.time()
         while np.abs(imbal) > 0.001:
             self.step()
-            imbal = (self.R_frc - self.lamparam*self.variables['T']).mean()
+            imbal = (self.R_frc - self.lamparam * self.variables["T"]).mean()
         end = time.time()
-        print('Done!')
-        print('Time = {} seconds'.format(end-start))
+        print("Done!")
+        print("Time = {} seconds".format(end - start))
